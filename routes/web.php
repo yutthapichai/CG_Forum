cf64ca062858a493487a8fce7a29a0022b076cbb
Yutthapichai <cancel_go@hotmail.com>
2018-08-12 21:35:24 +0700

    Create migration and model relationships

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('{provider}/auth', 'SocialsController@auth')->name('social.auth');
+
+Route::get('/{provider}/redirect', 'SocialsController@auth_callback')->name('social.callback');
